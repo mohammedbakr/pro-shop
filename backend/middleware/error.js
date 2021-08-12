757522c39e8d9f2325b06833863b84a4e2841899
Mohamed Bakr <bakr.engineer@gmail.com>
2021-08-12 05:34:58 +0200

    enhance errorHandler for not found routes

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,8 +1,7 @@
 import ErrorResponse from '../utils/errorResponse.js'
 
 const notFound = (req, res, next) => {
-  const error = new ErrorResponse(`Not Found - ${req.originalUrl}`)
-  res.status(404)
+  const error = new ErrorResponse(`Not Found - ${req.originalUrl}`, 404)
   next(error)
 }
 
