76285dde88af90a3f220e9efdb38d50e32c30400
Mohamed Bakr <bakr.engineer@gmail.com>
2021-08-25 22:51:53 +0200

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,15 @@ app.use('/api/v1/config', configRoutes)
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
+// For Production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+}
+
 // Error handler middlewares
 app.use(notFound)
 app.use(errorHandler)
